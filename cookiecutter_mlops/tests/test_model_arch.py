454e002752d616517fcc21091ece4e5e5e5398ec
RianLeevinson <rianleevinson@yahoo.com>
2022-01-18 21:47:30 +0100

    update_with_exercises

---
+++ cookiecutter_mlops/tests/test_model_arch.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+######################################################################
+# Authors:      <s202540> Rian Leevinson
+#                     <s202385> David Parham
+#                     <s193647> Stefan Nahstoll
+#                     <s210246> Abhista Partal Balasubramaniam
+#
+# Course:        Machine Learning Operations
+# Semester:    Spring 2022
+# Institution:  Technical University of Denmark (DTU)
+#
+# Module: This module is responsible for testing the model architecture
+######################################################################
+
+from src.models import model
+
+
+def test_model_arch():
+    """tests the model architecture"""
+    assert model.MyAwesomeModel()
+
+
